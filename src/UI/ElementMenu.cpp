07ede41e715b6be3a0b811d8afbd795503db1ff5
PalaceSwitcher <conmdelta@gmail.com>
2025-03-10 21:51:12 -0400

    Clean up element menu

--- src/UI/ElementMenu.cpp
+++ src/UI/ElementMenu.cpp
@@ -19,7 +19,6 @@ std::vector<std::string> matchingElemIDs; //Any elements matching the search que
 int elementMenuSpawnCount; //Number of elements spawned in the element menu. Used to determine where an element should be placed
 std::unordered_map<std::string, int> elementMenuSelectCounts; //The amount of times each element was selected in the element menu
 bool elementMenuOpen = false; //Whether element menu is opened or not
-bool elementSearchSelected; //Used to determine if an element is selected in the element search menu
 
 void UI::openElementMenu() {
 	elementMenuOpen = true;
@@ -38,6 +37,7 @@ std::vector<std::string> getElemSearchResults(std::string query, std::vector<std
 }
 
 void UI::renderElemMenu(std::vector<std::string> elementsUnlocked) {
+	float elemBoxSize = 64.0f;
 	float winWidth = ImGui::GetWindowWidth();
 	float winHeight = ImGui::GetWindowHeight();
 	if (elementMenuOpen) {
@@ -45,12 +45,13 @@ void UI::renderElemMenu(std::vector<std::string> elementsUnlocked) {
 		int rowCount = round((float)elementsUnlocked.size() / columnCount); //Get column and row count
 		//ImLerp(255.0f, 0.0f, (ImGui::GetTime() - start_time) / duration_seconds)
 		ImGui::SetNextWindowFocus();
-		ImGui::SetNextWindowSize(ImVec2(winWidth, winHeight), ImGuiCond_Always);
-		ImGui::SetNextWindowPos(ImVec2(winWidth*0.5, winHeight*0.5), ImGuiCond_Always, ImVec2(0.5f,0.5f)); //Center window
-		ImGui::PushStyleColor(ImGuiCol_WindowBg, IM_COL32(15,15,15,255)); //Set color
-		ImGui::Begin("Add Element", &elementMenuOpen, ImGuiWindowFlags_NoCollapse|ImGuiWindowFlags_NoResize);
-		ImGui::PushStyleColor(ImGuiCol_HeaderActive, IM_COL32(63,63,63,255));
-		ImGui::PushStyleColor(ImGuiCol_HeaderHovered, IM_COL32(95,95,95,255)); //Style selectables
+		ImGuiViewport* viewport = ImGui::GetMainViewport();
+		ImGui::SetNextWindowSize(viewport->WorkSize, ImGuiCond_Always);
+		ImGui::SetNextWindowPos(viewport->WorkPos, ImGuiCond_Always); //Center window
+		ImGui::PushStyleColor(ImGuiCol_WindowBg, IM_COL32(15,15,15,255)); //Set color of background
+		ImGui::PushStyleColor(ImGuiCol_HeaderHovered, IM_COL32(63,63,63,255));
+		ImGui::PushStyleColor(ImGuiCol_HeaderActive, IM_COL32(95,95,95,255));
+		ImGui::Begin("Add Element", &elementMenuOpen, ImGuiWindowFlags_NoCollapse|ImGuiWindowFlags_NoResize); //Style selectables
 		if (ImGui::InputTextWithHint("##elementMenuSearchBox", "Element", &elementMenuInputBuf, ImGuiInputTextFlags_CallbackEdit)) {
 			matchingElemIDs = getElemSearchResults(elementMenuInputBuf, elementsUnlocked);
 		}
@@ -61,28 +62,70 @@ void UI::renderElemMenu(std::vector<std::string> elementsUnlocked) {
 			}
 		}
 
-		if (!matchingElemIDs.empty() && ImGui::BeginTable("##elementMenuSearchGrid", columnCount)) {
-			for (auto id : matchingElemIDs) {
-				ImGui::TableNextColumn();
-				std::string elemName = Text::getElemName(id);
-				auto pos = ImGui::GetCursorPos(); //Get position of selectable box so items can be placed on it
-				std::string selectableId = "##" + id; //Unique ID for the element's selection box
-				if (ImGui::Selectable(selectableId.c_str(), elementSearchSelected, 0, ImVec2(64.0f, 64.0f))) {
-					elementMenuSelectCounts[id]++;
-					elementMenuSpawnCount++;
+		if (!matchingElemIDs.empty()) {
+			int colCount = floor(winWidth / elemBoxSize);
+			for (int i = 0; i < matchingElemIDs.size(); i++) {
+				if (i % colCount != 0) { ImGui::SameLine(); }
+				std::string elemName = Text::getElemName(matchingElemIDs[i]);
+				ImGui::PushID(i);
+				ImVec2 pos = ImGui::GetCursorPos(); //Get position of selectable box so items can be placed on it
+				if (ImGui::Selectable("##", false, ImGuiSelectableFlags_None, ImVec2(elemBoxSize, elemBoxSize))) {
+					int incAmount = 1;
+					if (ImGui::IsKeyDown(ImGuiKey_ModCtrl)) {
+						incAmount = 5;
+					}
+					if (elementMenuSelectCounts[matchingElemIDs[i]] + incAmount > 99) {
+						elementMenuSpawnCount += (99 - elementMenuSelectCounts[matchingElemIDs[i]]);
+						elementMenuSelectCounts[matchingElemIDs[i]] = 99;
+					} else {
+						elementMenuSpawnCount += incAmount;
+						elementMenuSelectCounts[matchingElemIDs[i]] += incAmount;
+					}
 				}
+				if (ImGui::IsItemClicked(ImGuiMouseButton_Right)) {
+					int decAmount = 1;
+					if (ImGui::IsKeyDown(ImGuiKey_ModCtrl)) {
+						decAmount = 5;
+					}
+					if (elementMenuSelectCounts[matchingElemIDs[i]] - decAmount < 0) {
+						elementMenuSpawnCount -= (decAmount - elementMenuSelectCounts[matchingElemIDs[i]]);
+						elementMenuSelectCounts[matchingElemIDs[i]] = 0;
+					} else {
+						elementMenuSpawnCount -= decAmount;
+						elementMenuSelectCounts[matchingElemIDs[i]] -= decAmount;
+					}
+				}
+				else if (ImGui::IsItemClicked(ImGuiMouseButton_Middle)) {
+					elementMenuSpawnCount -= elementMenuSelectCounts[matchingElemIDs[i]];
+					elementMenuSelectCounts[matchingElemIDs[i]] = 0;
+				}
+				auto afterPos = ImGui::GetCursorPos();
+				ImGui::PopID();
 				int elemW, elemH;
-				SDL_Texture* elemTex = Board::loadTexture(id, &elemW, &elemH); //Load texture and its dimensions
-				ImGui::SetCursorPos(ImVec2(pos.x, pos.y));
-				ImGui::Text("%d", elementMenuSelectCounts[id]); //Display the number of the element spawned
-				ImGui::SetCursorPos(ImVec2(pos.x+(64.0-elemW)/2, pos.y));
-				ImGui::Image((ImTextureID)(intptr_t) Board::textureIndex[id], ImVec2((float)elemW, (float)elemH));
-				ImGui::SetCursorPos(ImVec2(pos.x, pos.y+32));
-				ImGui::PushTextWrapPos(pos.x+64);
-				ImGui::TextWrapped("%s", elemName.c_str());
-				//ImGui::PopTextWrapPos();
+				SDL_Texture* elemTex = Board::loadTexture(matchingElemIDs[i], &elemW, &elemH); //Load texture and its dimensions
+				ImVec2 min = ImGui::GetItemRectMin();
+				ImVec2 max = ImGui::GetItemRectMax();
+				ImVec2 center = ImVec2(min.x + ceil((max.x - min.x - elemW) * 0.5f), min.y + (elemBoxSize * 0.125f));
+				ImDrawList* drawList = ImGui::GetWindowDrawList();
+				// Draw icon
+				drawList->AddImage((ImTextureID)(intptr_t) Board::textureIndex[matchingElemIDs[i]], center, ImVec2(center.x + elemW, center.y + elemH));
+				// Draw spawn count
+				std::string spawnCountTxt = std::to_string(elementMenuSelectCounts[matchingElemIDs[i]]);
+				ImVec2 textSize = ImGui::CalcTextSize(spawnCountTxt.c_str(), nullptr, false, elemBoxSize);
+				float spawnCountX = pos.x + (elemBoxSize - textSize.x);
+				float spawnCountY = pos.y;
+				drawList->AddText(ImGui::GetFont(), ImGui::GetFontSize(),
+									ImVec2(spawnCountX, spawnCountY),
+									IM_COL32(191,191,191,255),
+									spawnCountTxt.c_str(), nullptr, elemBoxSize);
+				textSize = ImGui::CalcTextSize(elemName.c_str(), nullptr, false, elemBoxSize);
+				float textStartX = pos.x + ((elemBoxSize - textSize.x) * 0.5f);
+				float textStartY = pos.y + (elemBoxSize * 0.375f) + textSize.y;
+				drawList->AddText(ImGui::GetFont(), ImGui::GetFontSize(),
+									ImVec2(textStartX, textStartY),
+									ImGui::GetColorU32(ImGuiCol_Text),
+									elemName.c_str(), nullptr, elemBoxSize);
 			}
-			ImGui::EndTable();
 		}
 		ImGui::PopStyleColor();
 		ImGui::PopStyleColor();
